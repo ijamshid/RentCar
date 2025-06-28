4a730956c6fd99d0661ba746277b71df29a2967f
ijamshid <ijamshid007@gamil.com>
2025-06-28 17:59:56 +0500

    entities

---
+++ src/RentCar.DataAccess/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RentalHome.Core.Entities;
+
+namespace RentalHome.DataAccess.Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        throw new NotImplementedException();
+    }
+}
