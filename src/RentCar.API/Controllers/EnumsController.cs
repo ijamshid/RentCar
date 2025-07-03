485ca0fda706c0805cd78c3c899454ecf99f3256
Zayd <zaydabduxamidov2008@gmail.com>
2025-07-03 18:51:19 +0500

    Controller for showin enums list.

---
+++ src/RentCar.API/Controllers/EnumsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentCar.Application.DTOs;
+using RentCar.Core.Enums;
+
+namespace RentCar.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnumsController : ControllerBase
+    {
+        public ActionResult<List<SelectListItemDto>> GetEnumSelectList([FromQuery] string enumName)
+        {
+            Type? enumType = enumName switch
+            {
+                "FuelType" => typeof(FuelType),
+                "PaymentSatus" => typeof(PaymentStatus),
+                "ReservationStatus" => typeof(ReservationStatus),
+                _ => null
+            };
+
+            if(enumType == null || !enumType.IsEnum)
+            {
+                return BadRequest("Invalid enum name provided.");
+            }
+
+            var list = Enum.GetValues(enumType)
+                .Cast<object>()
+                .Select(e => new SelectListItemDto
+                {
+                    Value = (int)e,
+                    Text = e.ToString() ?? ""
+                }).ToList();
+
+            return Ok(list);
+        }
+
+    }
+}
---
+++ src/RentCar.Application/DTOs/SelectListItemDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RentCar.Application.DTOs
+{
+    public class SelectListItemDto
+    {
+        public int Value { get; set; }
+        public string Text { get; set; }
+    }
+}
