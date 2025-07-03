ad9961594a9c28c4d8a488a83986b54eb91b6b39
Zayd <zaydabduxamidov2008@gmail.com>
2025-07-03 17:18:38 +0500

    Some changes done

---
+++ src/RentCar.Application/Mapping/BrandProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using RentCar.Application.DTOs;
+using RentCar.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentCar.Application.Mapping
+{
+    public class BrandProfile : Profile
+    {
+        public BrandProfile()
+        {
+            CreateMap<CreateBrandDto, Brand>();
+            CreateMap<Brand, BrandGetDto>();
+            CreateMap<UpdateBrandDto, Brand>();
+        }
+    }
+}
---
+++ src/RentCar.Application/Mapping/CarProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using RentCar.Application.DTOs;
+using RentCar.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentCar.Application.Mapping
+{
+    public class CarProfile : Profile
+    {
+        public CarProfile()
+        {
+            CreateMap<CreateBrandDto, Brand>();
+            CreateMap<Brand, BrandGetDto>();
+            CreateMap<UpdateBrandDto, Brand>();
+        }
+    }
+}
