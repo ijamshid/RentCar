604b267223ea8c823ad569e34f81abbde27a6f21
ijamshid <ijamshid007@gamil.com>
2025-07-10 18:42:27 +0500

    Add validation attributes to DTO classes
    Updated `CreateBrandDto` and `UpdateBrandDto` to enforce required fields and length constraints for `Name` and `CountryOfOrigin`. Enhanced `CarCreateDto` and `CarUpdateDto` with validation for multiple properties, ensuring data integrity. Modified `ReturnCarDto` to require `ReturnDate` and `CarMileage` with non-negative constraints. Refined `CreatePhotoModel` to include validation for `Url` and `CarId`, ensuring proper URL format and required fields.
    
    Add validation attributes to DTO classes
    
    Updated `CreateBrandDto` and `UpdateBrandDto` to enforce required fields and length constraints for properties. Enhanced `CarCreateDto` and `CarUpdateDto` with validation for multiple properties to ensure data integrity. Modified `ReturnCarDto` to require `ReturnDate` and `CarMileage` with constraints. Refined `CreatePhotoModel` to include validation for `Url` and `CarId`, ensuring proper URL format and required fields.

--- src/RentCar.Application/Models/Car/ReturnCarDto.cs
+++ src/RentCar.Application/Models/Car/ReturnCarDto.cs
@@ -1,13 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace RentCar.Application.Models.Car
 {
     public class ReturnCarDto
     {
-        [Required]
+        [Required(ErrorMessage = "Qaytarish sanasi majburiy")]
         public DateTime ReturnDate { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Avtomobilning yurgan masofasi majburiy")]
+        [Range(0, double.MaxValue, ErrorMessage = "Avtomobilning yurgan masofasi manfiy bo‘lishi mumkin emas")]
         public double CarMileage { get; set; }
     }
 }
